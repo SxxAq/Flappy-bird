3461f075e55aaf2321ae27ac93735d2cbc36d5e3
SxxAq <aqueel192@gmail.com>
2024-07-31 02:45:46 +0530

    fixes

--- flappyBird.js
+++ flappyBird.js
@@ -10,7 +10,10 @@ const birdHeight = 24;
 let birdX = boardWidth / 15;
 let birdY = boardHeight / 2;
 let birdImg;
+/*
 
+
+*/
 let bird = {
   x: birdX,
   y: birdY,
@@ -23,17 +26,29 @@ let pipeWidth = 64;
 let pipeHeight = 520;
 let pipeX = boardWidth;
 let pipeY = 0;
-
 // game physics
 let velocityX = -4; // pipe moving speed
 let velocityY = 0; // bird jump speed
 let gravity = 0.4;
-
 //score
 let score = 0;
 let numberImages = [];
+let gameStarted = false;
+let startImgLoad = false;
+let startImage = new Image();
+let topPipeImg = new Image();
+let botPipeImg = new Image();
 let gameOverImg = new Image();
 gameOverImg.src = "./assets/gameover.png";
+topPipeImg.src = "./assets/toppipe.png";
+startImage.src = "./assets/message.png";
+botPipeImg.src = "./assets/bottompipe.png";
+
+/*
+
+
+
+*/
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
@@ -51,24 +66,24 @@ window.onload = function () {
     ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
   };
 
-  topPipeImg = new Image();
-  topPipeImg.src = "./assets/toppipe.png";
-
-  botPipeImg = new Image();
-  botPipeImg.src = "./assets/bottompipe.png";
-  requestAnimationFrame(update);
   setInterval(placePipes, 1500);
   document.addEventListener("keydown", moveBird);
   document.addEventListener("click", moveBird);
 };
+/*
 
+
+
+
+*/
 function update() {
-  requestAnimationFrame(update);
-  if (checkCollisions()) {
-    drawGameOver();
+  ctx.clearRect(0, 0, board.width, board.height);
+
+  if (!gameStarted) {
+    drawStart()
     return;
   }
-  ctx.clearRect(0, 0, board.width, board.height);
+
   // bird
   velocityY += gravity;
   //   bird.y += velocityY;
@@ -76,6 +91,7 @@ function update() {
   ctx.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 
   if (checkCollisions()) {
+    drawGameOver();
     return;
   }
 
@@ -84,6 +100,12 @@ function update() {
     let pipe = pipeArray[i];
     pipe.x += velocityX;
     ctx.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    // Remove off-screen pipes
+    if (pipe.x + pipe.width < 0) {
+      pipeArray.splice(i, 1);
+      i--; // Adjust index after removal
+    }
 
     // Check if the bird has passed the pipe
     if (!pipe.passed && pipe.x + pipe.width < bird.x) {
@@ -94,8 +116,15 @@ function update() {
 
   //draw score
   drawScore();
+  requestAnimationFrame(update);
 }
+/*
 
+
+
+
+
+*/
 function placePipes() {
   if (checkCollisions()) {
     return;
@@ -122,8 +151,18 @@ function placePipes() {
   };
   pipeArray.push(botPipe);
 }
+/*
+
+
+
+
+*/
 
 function moveBird(e) {
+  if (!gameStarted) {
+    gameStarted = true;
+    requestAnimationFrame(update);
+  }
   if (
     e.type === "keydown" &&
     (e.code === "Space" ||
@@ -137,7 +176,10 @@ function moveBird(e) {
     velocityY = -6;
   }
 }
+/*
 
+
+*/
 function loadNumberImages() {
   for (let i = 0; i < 10; i++) {
     let img = new Image();
@@ -145,7 +187,11 @@ function loadNumberImages() {
     numberImages.push(img);
   }
 }
+/*
 
+
+
+*/
 function drawScore() {
   let scoreStr = score.toString();
   let startX = board.width - scoreStr.length * 20 - 10; // Start drawing from the right
@@ -156,6 +202,18 @@ function drawScore() {
     ctx.drawImage(img, startX + i * 20, 20, 20, 30); // Adjust the size and position as needed
   }
 }
+/*
+
+
+
+*/
+function drawStart() {
+  ctx.drawImage(
+    startImage,
+    (board.width - startImage.width) / 2,
+    (board.height - startImage.height) / 2
+  );
+}
 
 function drawGameOver() {
   ctx.drawImage(
@@ -164,7 +222,11 @@ function drawGameOver() {
     (board.height - gameOverImg.height) / 2
   );
 }
+/*
 
+
+
+*/
 function checkCollisions() {
   // Check if bird hits the ground or top
   if (bird.y + bird.height > boardHeight || bird.y < 0) {
